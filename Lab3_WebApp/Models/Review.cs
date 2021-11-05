e74617d14fc652016357a238f3536cf92c99a653
Ha <hanghenguyen@gmail.com>
2021-11-05 15:44:11 -0400

    edit review

--- Lab3_WebApp/Models/Review.cs
+++ Lab3_WebApp/Models/Review.cs
@@ -10,5 +10,15 @@ namespace Lab3_WebApp.Models
         public int Rating { get; set; }
         public string Comment { get; set; }
         public string DateTime { get; set; }
+
+        public Review(int rating, string comment, string datetime)
+        {
+            Rating = rating;
+            Comment = comment;
+            DateTime = datetime;
+        }
+        public Review()
+        { 
+        }
     }
 }
